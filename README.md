d7cdab422fb5e03fed086067431650e47ffe0011
Adam Callanan <a.callanan@sbcglobal.net>
2022-03-20 01:51:51 -0500

    update title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DSM Document Archive
+# IPIB Archive
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.3.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe("workspace-project App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual("DSM Document Archive");
+    expect(page.getTitleText()).toEqual("IPIB Archive");
   });
 
   afterEach(async () => {
